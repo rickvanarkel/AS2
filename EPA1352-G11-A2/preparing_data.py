3b841bb15d818a2f6f59ae13002ecd6c25f1ceba
sruinaard <125878309+sruinaard@users.noreply.github.com>
2023-03-02 15:04:56 +0100

    40% of bridges have a length and condition, first map of the N1

--- EPA1352-G11-A2/preparing_data.py
+++ EPA1352-G11-A2/preparing_data.py
@@ -1,4 +1,4 @@
-from G11_A2_run import df_roads, df_bridges
+#from G11_A2_run import df_roads, df_bridges
 
 print(df_roads.head(5))
 
@@ -19,10 +19,10 @@ def change_column_names():
     """
     df_roadN1.rename(columns={'type': 'model_type'}, inplace=True)
     df_roadN1['length'] = np.nan
-    df_roadN1['id'] = np.nan
+    df_roadN1['id'] = ''
     df_roadN1['name'] = ''
     df_roadN1['condition'] = ''
-    df_roadN1['road_name'] = ''
+    df_roadN1['road_name'] = 'Unknown'
 
 def change_model_type():
     """
@@ -33,9 +33,9 @@ def change_model_type():
     bridge_types = ['Bridge'] # in doubt over: CrossRoad, RailRoadCrossing, Culvert
 
     for i in bridge_types:
-        df_roadN1.loc[df_roadN1['model_type'].str.contains(i), 'type'] = 'bridge'
+        df_roadN1.loc[df_roadN1['model_type'].str.contains(i), 'model_type'] = 'bridge'
 
-    df_roadN1.loc[~df_roadN1['model_type'].str.contains('bridge'), 'type'] = 'road'
+    df_roadN1.loc[~df_roadN1['model_type'].str.contains('bridge'), 'model_type'] = 'road'
 
     df_roadN1['model_type'].iloc[0] = 'source'
     df_roadN1['model_type'].iloc[-1] = 'sink'
@@ -60,8 +60,8 @@ def connect_infra():
             if not matching_bridge.empty:
                 bridge_condition = matching_bridge.iloc[0]['condition']
                 bridge_length = matching_bridge.iloc[0]['length']
-                df_roadN1.at[index, 'bridge_condition'] = bridge_condition
-                df_roadN1.at[index, 'bridge_length'] = bridge_length
+                df_roadN1.at[index, 'condition'] = bridge_condition
+                df_roadN1.at[index, 'length'] = bridge_length
 
     # find less exact match between road+LRP (or another way, sustain it, what will work?)
     # what do we need to do with chainage?
@@ -69,7 +69,22 @@ def connect_infra():
     df_roadN1_bridges = df_roadN1[df_roadN1['model_type'] == 'bridge']
     #print(df_roadN1_bridges[['road_id', 'model_type', 'bridge_condition', 'bridge_length']].head(5))
     #print(df_roadN1_bridges.isnull().sum())
-    df_roadN1.to_excel('check_N1_df.xlsx')
+
+
+def get_length():
+    pass
+
+def get_name():
+    df_roadN1['name'] = df_roadN1['model_type']
+
+def get_road_name():
+    pass
+
+def make_id():
+    unique_id = 1000000
+    for i in range(len(df_roadN1['id'])):
+        df_roadN1.loc[i, 'id'] = unique_id
+        unique_id += 1
 
 def make_figure():
     # make a figure of N1
@@ -82,6 +97,15 @@ def prepare_data():
     change_column_names()
     change_model_type()
     connect_infra()
+    get_length()
+    get_name()
+    get_road_name()
+    make_id()
     make_figure()
 
 prepare_data()
+
+for i in df_roadN1:
+    print(i)
+
+df_roadN1.to_excel('check_N1_df.xlsx')
