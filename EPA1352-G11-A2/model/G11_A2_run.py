13af4217c228f91b1b6887cdfff675fbb1c9c5a9
superjager1 <daan@jagernet.nl>
2023-03-09 13:11:14 +0100

    Volledige model ellende, moet alleen nog comments bij en aan elkaar geplakt worden.

---
+++ EPA1352-G11-A2/model/G11_A2_run.py
@@ -0,0 +1,36 @@
+import pandas as pd
+import warnings
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+warnings.filterwarnings('ignore')
+
+'''
+This file is the main run file of our model
+'''
+
+def import_file(filepath):
+    '''
+    This function is used to import files form a selected filepath
+    It returns a variable with the read file
+    It is currently only possible to import filetypes 'csv' or 'xlsx'
+    '''
+    dataframe = ""
+    try:
+        dataframe = pd.read_csv(filepath)
+    except:
+        dataframe = pd.read_excel(filepath)
+    return dataframe
+
+df_roads = import_file('./data/_roads3.csv')
+df_bridges = import_file('./data/BMMS_overview.xlsx')
+
+exec(open("preparing_data.py").read())
+
+'''
+This function runs the model_run file, this file automatically runs all the scenario's that..
+were discribed in the assignment. The current settings are set to LB mode, for UB mode please...
+change the file output name and the file input name in the model.py and model_run.py files.
+'''
+exec(open("model_run.py").read())
+
---
+++ EPA1352-G11-A2/model/model_run.py
@@ -0,0 +1,36 @@
+import pandas as pd
+import random
+import model
+from components import Source
+
+"""
+    Run simulation
+    Print output at terminal
+"""
+
+# ---------------------------------------------------------------
+
+run_length = 5 * 24 * 60
+
+for i in range(9):
+
+    df_list = pd.DataFrame(columns=["run", "avarage"])
+    df_list.set_index("run", inplace=True)
+
+    for k in range(10):
+
+        seed = random.randint(1,1234567)
+        sim_model = model.BangladeshModel(seed=seed, scenario=i)
+        # Check if the seed is set
+        print("SEED " + str(sim_model._seed))
+
+        # One run with given steps
+        for j in range(run_length):
+            sim_model.step()
+        average_time = sim_model.reporter['Time'].mean()
+        df_list.loc[k] = [average_time]
+        Source.truck_counter = 0
+
+    df_list.to_csv(f'./data/LBscenario{i}.csv'
+    #df_list.to_csv(f'./data/UBscenario{i}.csv')
+
